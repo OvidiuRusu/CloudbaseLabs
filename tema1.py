543bb8121522b92c7ea28b5b4b00892d8bed9a5b
Ovidiu Rusu <orusu@cloudbasesolutions.com>
2016-04-18 13:12:39 +0300

    Tema 2

--- tema1.py
+++ tema1.py
@@ -1,70 +1,88 @@
 from collections import defaultdict
 
-def add_book():
-	global inventory
-	author = raw_input("Author:")
-	title = raw_input("Title:")
-	copies = raw_input("Number of copies:")
-	inventory[author + '@' + title] += ['available'] * int(copies)
-	print inventory
+class Book(object):
 
+	def __init__(self, author, title, copies=1):
+		self.author = author
+		self.title = title
+		self.copies = copies
+		self.lent = []
 
-def add_copy():
-	global inventory
-	author = raw_input("Author:")
-	title = raw_input("Title:")
-	status = raw_input("Status:")
-	inventory[author + '@' + title].append(status)
-	print inventory
+	def add(self):
+		self.copies += 1
 
-def delete_book():
-	global inventory
-	author = raw_input("Author:")
-	title = raw_input("Title:")
-	if inventory.get(author + '@' + title, 0) == 0:
-		print "Book does not exist in inventory"
-	else:	
-		del inventory[author + '@' + title]
-	print inventory
+	def remove(self):
+		if self.copies == 1:
+			raise Exception("There is only one copy left.")
+		self.copies -= 1
 
-def delete_copy():
-	global inventory
-	author = raw_input("Author:")
-	title = raw_input("Title:")
-	if inventory.get(author + '@' + title, 0) == 0:
-		print "Book does not exist in inventory"
-	else:		
-		if len(inventory[author + '@' + title]) == 1:
-			del inventory[author + '@' + title]
-		else:	
-			inventory[author + '@' + title].remove('available')
-	print inventory
+	def lend(self, person):
+		if len(self.lent) < self.copies:
+			self.lent.append(person)
+		else:
+			raise Exception("There are no books available.")
 
-def books_after_author(inventory, author):
-	books = inventory.keys()
-	for item in books:
-		if item.split('@')[0] == author:
-			print item.split('@')[1]
+	def restore(self, person):
+		self.lent.remove(person)
 
-def books_after_word_in_title(inventory, word):
-	books = inventory.keys()
-	for item in books:
-		if word in item.split('@')[1]:
-			print item.split('@')[0], ':' , item.split('@')[1]
+	def get_persons(self):
+		return self.lent	
 
+	def __repr__(self):
+		return "Author: {author}\nTitle: {title}\nNumber of copies: {copies}\nLent: {lent}\n".format(author=self.author, title=self.title, copies=self.copies, lent = self.lent)
 
-def update_status_book():
-	global inventory
-	author = raw_input("Author:")
-	title = raw_input("Title:")
-	copy = int(raw_input("Number of copy:"))
-	status = raw_input("Status:")
-	inventory[author + '@' + title][copy] = status
-	print inventory
+	def __str__(self):
+		return self.__repr__()	
+
+				
+class Inventory(object):
+	def __init__(self, database={}):
+		self.database = database
+
+	def add_book(self, author, title, copies):
+		self.database[author + '@' + title] = Book(author, title, copies)
+
+	def remove_book(self, author, title=None):
+		if author and title:
+			if self.database.get(author + '@' + title):
+				del self.database[author + '@' + title]
+			else:
+				print "Book does not exist in database."
+		else:		
+			for key, book in self.find(author):
+				del self.database[key]
+
+	def find(self, author=None, title=None):
+		if author:
+			return filter(lambda x: x.startswith(author + '@'), self.database)
+		elif title:
+			return filter(lambda x: title in x.split('@')[1], self.database)
+
+	def get_book(self, author, title):
+		return self.database.get(author + '@' + title, None)
+
+	def books_lent_to(self, person):
+		for book in self.database.values():
+			if person in book.get_persons():
+				print book.author, ':', book.title
+
+	def book_holders(self, author, title):
+		book = self.database.get(author + '@' + title, None)
+		if book:
+			print book.lent
+
+	def __repr__(self):
+		result = ""
+		for book in self.database.values():
+			result += str(book)
+		return result
+
+	def __str__(self):
+		return self.__repr__()		
 
 
 if __name__ == "__main__":
-	inventory = defaultdict(list)
+	inventory = Inventory()
 	while True:
 		print "1. Add a book"
 		print "2. Add a copy"
@@ -72,28 +90,64 @@ if __name__ == "__main__":
 		print "4. Delete_copy"
 		print "5. Search books after author"
 		print "6. Search books after a word in title"
-		print "7. Update status book"
-		print "8. Exit"
+		print "7. Lend a book"
+		print "8. Restore a book"
+		print "9. What books has a person"
+		print "10. Persons who hold a book"
+		print "11. Exit"
 		option = int(raw_input("Your option:"))
 		if option == 1:
-			add_book()
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			copies = int(raw_input("Number of copies:"))
+			inventory.add_book(author, title, copies)
+			print inventory
 		elif option == 2:
-			add_copy()
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			inventory.get_book(author, title).add()
+			print inventory
 		elif option == 3:
-			delete_book()
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			inventory.remove_book(author, title)
+			print inventory
 		elif option == 4:
-			delete_copy()
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			inventory.get_book(author, title).remove()
+			print inventory
 		elif option == 5:
 			author = raw_input("Author:")
-			books_after_author(inventory, author)	
+			bookitems = inventory.find(author)
+			for book in bookitems:
+				print "Book:", book.split('@')[1]
 		elif option == 6:
 			word = raw_input("Word:")
-			books_after_word_in_title(inventory, word)
-		elif option == 7:
-			update_status_book()
-		elif option == 8:		
+			bookitems = inventory.find(title=word)
+			for book in bookitems:
+				print "Book:", book.split('@')[1], "; Author:", book.split('@')[0]
+		elif option == 7:		
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			person = raw_input("Person:")
+			book = inventory.get_book(author, title)
+			book.lend(person)
+			print book
+		elif option == 8:
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			person = raw_input("Person:")
+			book = inventory.get_book(author, title)
+			book.restore(person)
+			print book
+		elif option == 9:
+			person = raw_input("Person:")
+			inventory.books_lent_to(person)
+		elif option == 10:
+			author = raw_input("Author:")
+			title = raw_input("Title:")
+			inventory.book_holders(author, title)
+		elif option == 11:
 			break	
-
-
-
-
+				
